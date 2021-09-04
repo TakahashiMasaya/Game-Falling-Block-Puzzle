bbfdf8b01881656276b55bd5cff2450391b0140d
Takahashi Masaya <ma.taka.ic@icloud.com>
2021-09-04 13:08:35 +0900

    currect collisiondirector. and its test

--- src/application/CollisionalDetector.ts
+++ src/application/CollisionalDetector.ts
@@ -1,4 +1,4 @@
-import { paramIsCollision } from '@/type/Application';
+import { paramIsCollision, ActiveTetrominoStatus } from '@/type/Application';
 
 export class CollisionalDetector {
   /**
@@ -15,9 +15,9 @@ export class CollisionalDetector {
     tetromino,
     field,
   }): boolean => {
-    const { x, y, tetromino: t } = tetromino;
-    const fieldCols = field[0].length - 1;
-    const fieldRows = field.length - 1;
+    const { x, y, tetromino: t }: ActiveTetrominoStatus = tetromino;
+    // const fieldCols = field[0].length - 1;
+    // const fieldRows = field.length - 1;
     // tetrominoの各bit値で衝突判定する
     for (let i = 0; i < t.length; i += 1) {
       // row
@@ -25,34 +25,46 @@ export class CollisionalDetector {
       for (let j = 0; j < row.length; j += 1) {
         // bit
         const bit: string = row[j];
-        // 左端
-        if (bit !== '0' && x + j < 0) {
-          return true;
-        }
-        // 右端
-        if (bit !== '0' && x + j > fieldCols) {
-          return true;
-        }
-        // 下端
-        if (bit !== '0' && y + i > fieldRows) {
-          return true;
-        }
+        // // 左端
+        // if (bit !== '0' && x + j < 0) {
+        //   return true;
+        // }
+        // // 右端
+        // if (bit !== '0' && x + j > fieldCols) {
+        //   return true;
+        // }
+        // // 下端
+        // if (bit !== '0' && y + i > fieldRows) {
+        //   return true;
+        // }
 
         // field内、pieceとの衝突判定
-        if (x + j >= 0
-          && x + j < fieldCols
-          && y + i >= 0
-          && y + i < fieldRows) {
-          // field内である
-          if (bit !== '0' && field[y + i][x + j] !== '0') {
-            return true;
-          }
+        // if (x + j >= 0
+        //   && x + j <= fieldCols
+        //   && y + i >= 0
+        //   && y + i <= fieldRows) {
+        // field内である
+        if (bit !== '0'
+          && field[y + i]
+          && field[y + i][x + j]
+          && field[y + i][x + j] !== '0') {
+          return true;
         }
+        // }
       }
     }
     return false;
   }
 
+  /**
+   * field・pieceの衝突判定
+   *
+   * @param {paramIsCollision} {
+   *     tetromino,
+   *     field,
+   *   }
+   * @memberof CollisionalDetector
+   */
   public isCollision = ({
     tetromino,
     field,
@@ -62,5 +74,44 @@ export class CollisionalDetector {
       field,
     });
     return isFieldCollision;
+  }
+
+  /**
+   * tetrominoの着座判定
+   *
+   * @param {paramIsCollision} {
+   *     tetromino,
+   *     field,
+   *   }
+   * @memberof CollisionalDetector
+   */
+  public isTetrominoReached = ({
+    tetromino,
+    field,
+  }: paramIsCollision): boolean => {
+    const { x, y, tetromino: t } = tetromino;
+    // const fieldRows = field.length - 1;
+    // tetrominoの各bit値で衝突判定する
+    for (let i = 0; i < t.length; i += 1) {
+      // row
+      const row: string[] = t[i];
+      for (let j = 0; j < row.length; j += 1) {
+        // bit
+        const bit: string = row[j];
+        // // 下端
+        // if (bit !== '0' && y + i > fieldRows) {
+        //   return true;
+        // }
+
+        // field内、pieceとの衝突判定
+        // if (y + i <= fieldRows) {
+        // field内である
+        if (bit !== '0' && field[y + i][x + j] !== '0') {
+          return true;
+        }
+        // }
+      }
+    }
+    return false;
   }
 }
